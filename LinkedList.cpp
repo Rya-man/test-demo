30044cb139c899236ef7912cb1e410d0d3955061
Aryaman Prakash <arpr234@gmail.com>
2023-11-01 14:22:12 +0530

    update

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,15 +5,34 @@ class node
     public:
     int data;
     node* next;
-    node(){
-        data = 10;
-        next = NULL;
-    }
-
+    //node(){
+      //  data = 10;
+        //next = NULL;
+    //}
+    node(int s=0,node* f=NULL)
+    {
+        data = s;
+        next = f;
+    }  
 };
+void traversal(node* head)
+{
+    while(head)
+    {
+        cout<<head->data<<' ';
+        head=head->next;
+    }
+}
 int main()
 {
-    node *n1 = new node();
-    cout<<n1->data;
+    node *head,*n1,*n2;
+    head = new node();
+    n1= new node(1);
+    n2=new node(2);
+    head->next= n1;
+    n1->next=n2;
+    cout<<'\n';
+    traversal(head);
+
 
 }  
