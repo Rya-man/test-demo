82363d45758619129ec10e25440ca707a188960e
Aryaman Prakash <arpr234@gmail.com>
2023-11-27 17:18:22 +0530

    Commit changes

--- sample2.c
+++ sample2.c
@@ -1,58 +1,68 @@
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-#include<ctype.h>
-#include<string.h>
-struct Employee
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Employee {
     char Emp_Name[100];
     char Emp_id[10];
-    char Emp_department[3];
+    char Emp_department[50];
     float Emp_salary;
-
 };
-void readdata(struct Employee *a){
-    static int x =1;
-    printf("\nENter name %d\t",x);
-    scanf("%s",a->Emp_Name);
-    printf("ENter id %d\t",x);
-    scanf("%s",a->Emp_id);
-    printf("ENter department %d\t",x);
-    scanf("%s",a->Emp_department);
-    printf("ENter salary %d\t",x);
-    scanf("%f",&a->Emp_salary);
-    x++;
+
+void readdata(struct Employee *a, int index) {
+    printf("\nEnter details for Employee %d:\n", index + 1);
+    printf("Enter name: ");
+    scanf("%s", a->Emp_Name);
+    printf("Enter id: ");
+    scanf("%s", a->Emp_id);
+    printf("Enter department: ");
+    scanf("%s", a->Emp_department);
+    printf("Enter salary: ");
+    scanf("%f", &a->Emp_salary);
 }
-float total(struct Employee *a,char dep[3])
-{
-    if(!strcmp(a->Emp_department,dep))
-    {
-        return a->Emp_salary;
+
+float total(struct Employee *a, int n, char dep[]) {
+    float sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (strcmp(a[i].Emp_department, dep) == 0) {
+            sum += a[i].Emp_salary;
+        }
     }
-    else return 0;
+    return sum;
 }
-void display(struct Employee *a)
-{
-    static int df = 1;
-    printf("\n\nEmployee %d Name :\t%s\nEmployee %d id :\t%s\nEmployee %d Department :\t%s\nEmployee %d Salary :\t%f\n",df,a->Emp_Name,df,a->Emp_id,df,a->Emp_department,df,a->Emp_salary); 
+
+void display(struct Employee *a, int n) {
+    printf("\nEmployee Details:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Employee %d Name: %s\nEmployee %d id: %s\nEmployee %d Department: %s\nEmployee %d Salary: %f\n\n",
+               i + 1, a[i].Emp_Name, i + 1, a[i].Emp_id, i + 1, a[i].Emp_department, i + 1, a[i].Emp_salary);
+    }
 }
-int main()
-{
-    struct Employee a[10];
-    int n,sum=0;char dep[3];
-    printf("ENTER NUMBER OF EMPLOYEES\n"); scanf("%d",&n);
-    for(int i=0;i<n;i++)
-    {
-        readdata(&a[i]);
-    //   display(&a);
+
+int main() {
+    int n;
+    printf("Enter number of employees: ");
+    scanf("%d", &n);
+
+    struct Employee *employees = malloc(n * sizeof(struct Employee));
+    if (employees == NULL) {
+        printf("Memory allocation failed.\n");
+        return -1;
     }
-    printf("\nenter department to find the total\t");
-    scanf("%s",dep);
-    for(int i=0;i<n;i++)
-    {
-        sum+=total(&a[i],dep);
+
+    for (int i = 0; i < n; i++) {
+        readdata(&employees[i], i);
     }
-    printf("\nTotal for this department is %f",sum);
+
+    char dep[50];
+    printf("\nEnter department to find the total salary: ");
+    scanf("%s", dep);
+
+    float total_salary = total(employees, n, dep);
+    printf("\nTotal salary for department %s: %.2f\n", dep, total_salary);
+
+    display(employees, n);
+
+    free(employees); // Free dynamically allocated memory
     return 0;
-//printf("%f",a->Emp_salary);
 }
