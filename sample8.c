5dba67ad4626f1336742dbb2b7222a69019c19a1
Aryaman Prakash <arpr234@gmail.com>
2024-01-29 20:14:22 +0530

    commit changes

--- sample8.c
+++ sample8.c
@@ -18,7 +18,7 @@ struct Circularqueue* createqueue(int n) {
 }
 
 void insert(int value, struct Circularqueue* queue) {
-    if ((queue->front == 0 && (queue->rear + 1) % queue->maxsize == queue->front) || ((queue->rear + 1) % queue->maxsize == queue->front)) {
+    if ((queue->front == 0 && (queue->rear + 1) % queue->maxsize == 0) || ((queue->rear + 1) % queue->maxsize == queue->front)) {
         printf("Queue is full\n");
         return;
     }
@@ -34,8 +34,8 @@ void insert(int value, struct Circularqueue* queue) {
 int delete(struct Circularqueue* queue) {
     int val;
     if (queue->front == -1) {
-        printf("Queue underflow\n");
-        return -1;
+        // printf("Queue underflow\n");
+        return INT_MIN;
     }
     val = queue->data[queue->front];
     if (queue->front == queue->rear) {
@@ -48,17 +48,33 @@ int delete(struct Circularqueue* queue) {
 }
 
 void display(struct Circularqueue* queue) {
-    int i;
+    struct Circularqueue temp =*queue;
     if (queue->front == -1)
+    {
         printf("\nEmpty queue\n");
-    else {
-        printf("\nFront = %d\n", queue->front);
-        printf("Items:\t");
-        for (i = queue->front; i != (queue->rear + 1) % queue->maxsize; i = (i + 1) % queue->maxsize) {
-            printf("%d ", queue->data[i]);
-        }
-        printf("\nRear = %d\n\n", queue->rear);
+        return;
     }
+    printf("\nFront = %d\nItems:\t", queue->front);
+    do
+    {
+        printf(" %d",delete(&temp));
+    }while(temp.rear-temp.front!=0);
+    int val = delete(&temp);
+    if(val==INT_MIN){}
+    else
+        printf(" %d",val);
+    printf("\nRear = %d\n\n", queue->rear);
+    // int i;
+    // if (queue->front == -1)
+    //     printf("\nEmpty queue\n");
+    // else {
+    //     printf("\nFront = %d\n", queue->front);
+    //     printf("Items:\t");
+    //     for (i = queue->front; i != (queue->rear + 1) % queue->maxsize; i = (i + 1) % queue->maxsize) {
+    //         printf("%d ", queue->data[i]);
+    //     }
+    //     printf("\nRear = %d\n\n", queue->rear);
+    // }
 }
 
 int main() {
@@ -77,8 +93,10 @@ int main() {
             insert(value, queue);
         } else if (choice == 2) {
             value = delete(queue);
-            if (value != -1)
+            if (value != INT_MIN)
                 printf("\ndeleted value = %d\n", value);
+            else
+                printf("\nQueue Underflow\n");
         } else if (choice ==3) {
             display(queue);
         } else if (choice == 4) {
