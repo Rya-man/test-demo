cc9095ef295087df9cee622e2cd49205da667d7b
Aryaman <arpr234@gmail.com>
2024-06-18 23:10:49 +0530

    prim

--- arya.cpp
+++ arya.cpp
@@ -1,5 +1,5 @@
-#include<time.h>
 #include <bits/stdc++.h>
+#include <time.h>
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
@@ -7,7 +7,7 @@ using namespace std;
 
 // Typedefs
 #define ll long long
-#define nl cout<<'\n';
+#define nl cout << '\n';
 #define vi vector<int>
 #define pii pair<int, int>
 
@@ -16,16 +16,16 @@ const int MOD = 1e9 + 7;
 const int INF = INT_MAX;
 
 // Macros
-#define show(arr) for (auto i: arr) {cout << i << ' ';} cout << "\n"
-#define inp(arr,n) for(int i=0;i<n;i++) {cin>> arr[i];}
-#define all(v) (v).begin(),(v).end()
-#define rall(v) (v).rbegin(),(v).rend()
+#define show(arr) for (auto i : arr) { cout << i << ' '; } cout << "\n"
+#define inp(arr, n) for (int i = 0; i < n; i++) { cin >> arr[i]; }
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
 #define pb push_back
 #define mp make_pair
 #define F first
 #define S second
-#define yes cout<<"YES\n";
-#define no cout<<"NO\n";
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
 // Debugging
 #ifdef LOCAL_DEBUG
 #define debug(x) cerr << #x << " = " << x << endl;
@@ -34,12 +34,12 @@ const int INF = INT_MAX;
 #endif
 
 // Fast I/O
-auto initialize = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
+// auto initialize = []() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     return 0;
+// }();
 
 // Modular arithmetic
 ll mod(ll a, ll m) {
@@ -47,7 +47,7 @@ ll mod(ll a, ll m) {
 }
 
 // Power function with modulo
-ll power(ll x, ll y, ll m){
+ll power(ll x, ll y, ll m) {
     if (y == 0)
         return 1;
     ll p = power(x, y / 2, m) % m;
@@ -69,64 +69,81 @@ bool isPrime(int n) {
     return true;
 }
 
-ll sum_of_array(vi arr,int n)
-{
-    ll sum =0;
-    for(int i=0;i<n;i++)
-        sum+=arr[i];
+ll sum_of_array(vi arr, int n) {
+    ll sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
     return sum;
 }
 
-inline void insert_edges(int a,int b,vector<vector<pair<int,int>>>& adj,int c)
-{
-        adj[a].push_back(make_pair(b,c));
-        adj[b].push_back(make_pair(a,c));
+inline void insert_edges(int a, int b, vector<vector<pair<int, int>>>& adj, int c) {
+    adj[a].pb(mp(b, c));
+    adj[b].pb(mp(a, c));
 }
-void prim(vi&*,unordered_set<int>
-void result(){
-        int n;
-        cout<<"Enter the number of vertices\t";
-        cin>>n;
-        vector<vector<pair<int,int>>> adj(n);
-        int e;
-        cout<<"\nEnter the numebr of edges\t";
-        cin>>e;
-        cout<<"\n enter all edges with weight";
-        while(e)
-        {
-                int a,b,c;
-                cin>>a>>b>>c;
-                insert_edges(a,b,adj,c);
-                e--;
-        }
-        vector<int> res;
-        int weight =0;
-        priority_queue<pair<int,int>> pq;
-        unordered_set<int> vis;
-        for(int i =0;i<n;++i)
-        {
-                if(!vis.count(i))
-                        prim(res,vis,i,adj,weight,pq);
-        }
-        cout<<"\n\n";
 
-        show(res);
-        cout<<"\n weight = "<<weight<<"\n\n";
+void prim(vector<pii>& res, unordered_set<int>& vis, int i, vector<vector<pii>>& adj, int& weight, priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>>& pq) {
+    vis.insert(i);
+    for (auto [to, cost] : adj[i]) {
+        if (!vis.count(to)) {
+            pq.push(make_tuple(cost, i, to));
+        }
+    }
+
+    while (!pq.empty()) {
+        auto [cost, from, to] = pq.top();
+        pq.pop();
+        if (!vis.count(to)) {
+            vis.insert(to);
+            res.pb(mp(from, to));
+            weight += cost;
+            for (auto [next, next_cost] : adj[to]) {
+                if (!vis.count(next)) {
+                    pq.push(make_tuple(next_cost, to, next));
+                }
+            }
+        }
+    }
+}
+
+void result() {
+    int n;
+    cout << "Enter the number of vertices\t";
+    cin >> n;
+    vector<vector<pii>> adj(n);
+    int e;
+    cout << "\nEnter the number of edges\t";
+    cin >> e;
+    cout << "\nEnter all edges with weights (format: a b c)\n";
+    while (e--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        insert_edges(a, b, adj, c);
+    }
+
+    vector<pii> res;
+    int weight = 0;
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+    unordered_set<int> vis;
+
+    for (int i = 0; i < n; ++i) {
+        if (!vis.count(i)) {
+            prim(res, vis, i, adj, weight, pq);
+        }
+    }
+
+    cout << "\nMinimum Spanning Tree edges:\n";
+    for (auto i : res) {
+        cout << i.F << " -> " << i.S << '\n';
+    }
+
+    cout << "\nTotal weight = " << weight << "\n\n";
 }
 
 int main() {
-
     int tc;
-    cin>>tc;
-    while(tc)
-    {
+    cin >> tc;
+    while (tc--) {
         result();
-        tc--;
     }
-
-    // Your code here
-
     return 0;
 }
-
-
