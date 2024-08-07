75abeb3df1325a45a06cfeaafc3eabc51aaa254a
Aryaman <arpr234@gmail.com>
2024-08-07 19:19:38 +0530

    .

--- mkcpp.sh
+++ mkcpp.sh
@@ -1,22 +1,10 @@
 cd $1
 
-i=1
-mkdir partA
-cd partA
-while [ $i -le 7 ]
+i=0
+while [ $i -le 12 ]
 do
 	mkdir 'pgrm'$i
 	cp  $2 'pgrm'$i/$i.cpp
 	i=$(($i + 1))
 done
-cd ../
-mkdir partB
-cd partB
 
-i=1
-while [ $i -le 6 ]
-do
-	mkdir 'pgrm'$i
-	cp  $2 'pgrm'$i/$i.cpp
-	i=$(($i + 1))
-done
