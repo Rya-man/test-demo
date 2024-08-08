9360651a68f1a588c658c0c9a73cc228472a32ec
Aryaman Prakash <123716170+Rya-man@users.noreply.github.com>
2024-08-08 17:15:55 +0530

    Update binsearch.cpp

--- Lab internals/binsearch.cpp
+++ Lab internals/binsearch.cpp
@@ -161,53 +161,98 @@ inline void OPEN (string s) {
     #endif
 }
 
-int helper(vi&nums,int a,int i)
+int linsearch_iterative(vi& nums,int find)
+{
+    FOR(i,0,nums.size())
+    {
+        if(nums[i]==find)
+            return i;
+    }
+    return -1;
+}
+int linsearch_recursive(vi&nums,int& find,int i)
 {
     if(i>=nums.size())
     return -1;
-    if(nums[i]==a)
+
+    if(nums[i]==find)
     return i;
-    return helper(nums,a,i+1);
+
+    return linsearch_recursive(nums,find,i+1);
+
 }
-pair<int,double> binsearch(vi&nums,int a)
+int binsearch_iterative(vi&nums,int find)
 {
-    clock_t a1 =clock();
-    int l =0,r=nums.size()-1;
-    int loc = -1;
+    int l = 0;
+    int r = nums.size()-1;
+    int m = l + ((r-l)>>1);
     while(l<=r)
     {
-        int m = l+ ((r-l)>>1);
-        if(nums[m]==a)
-        {
-            loc=m;
-            break;
-        }
-        else if(nums[m]>a)
-        r=m-1;
+        int m = l + ((r-l)>>1);
+        if(nums[m]==find)
+        return m;
+        if(nums[m]>find)
+            r = m-1;
         else
-        l=m+1;
+            l=m+1;
     }
-    clock_t a2 =clock();
-    return {loc,(double)a2-a1/CLOCKS_PER_SEC*1000};
+    return -1;
 }
-void result(){
+int binsearch_recursive(vi&nums,int find,int& l,int& r)
+{
+    if(l>r)
+        return -1;
+    if(nums[(l + ((r-l)>>1))]==find)
+        return l + ((r-l)>>1);
+
+    if(nums[(l + ((r-l)>>1))]>find)
+        r = (l + ((r-l)>>1))-1;
+    else
+        l = (l + ((r-l)>>1))+1;
+
+    return binsearch_recursive(nums,find,l,r);    
+}
+void result() {
+    // Placeholder for result processing function
     int n;
+    cout<<"Enter the size of the array \t";
     cin>>n;
     vi nums(n);
     inp(nums,n);
-    int a;
-    cin>>a;
-    auto r = binsearch(nums,a);
-    cout<<"index = \t"<<r.first;
-    cout<<"\ntime = \t"<<r.second;
+
+    int searchelement;
+    cin>>searchelement;
+    int a,b;
+    cout<<"1. Linear search Iterative\n2. Linear Search recursive\n3.Binary Search iterative\n4.Binary Search recursive\t";cin>>a;nl
+    int l = 0;
+    int r =nums.size()-1;
+    switch (a)
+    {
+    case 1:
+        b=linsearch_iterative(nums,searchelement);
+        break;
+    case 2:
+        b=linsearch_recursive(nums,searchelement,0);
+        break;
+    case 3:
+        SORT(nums);
+        b=binsearch_iterative(nums,searchelement);
+        break;
+    case 4:
+        SORT(nums);
+        b=binsearch_recursive(nums,searchelement,l,r);
+        break;
+
+    default:
+        break;
+    }
+    cout<<"\n\nResult = "<<b;nl;
 }
 
 int main() {
-
     int tc;
-    cin>>tc;
-    TC(tc)
-    {
+    cin >> tc;
+    TC(tc){
         result();
     }
     return 0;
